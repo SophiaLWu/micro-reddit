9a507e92de0aa736e40e7e1e872227a0987ee5d5
Sophia <swoozie14.com>
2017-01-17 15:22:24 -0800

    Complete Comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, length: { minimum: 1, maximum: 100 },
                     uniqueness: { case_sensitive: false }
   validates :body, presence: true, length: { minimum: 1 }
