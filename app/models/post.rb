5e62805dd2de294a5f5ab94dd13ab2287c9519ce
Sophia <swoozie14.com>
2017-01-17 15:12:37 -0800

    Complete User and Post models

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, length: { minimum: 1, maximum: 100 },
+                    uniqueness: { case_sensitive: false }
+  validates :body, presence: true, length: { minimum: 1 }
+  validates :user_id, presence: true
+end
